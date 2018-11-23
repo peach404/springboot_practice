ac1b1a9653bf5fa87b2b3e5bd56ee8fb665b68b0
soyeon <peach404@naver.com>
2018-11-23 22:43:11 +0900

    [ADD]실습1

---
+++ src/main/java/me/soyeon/springpractice/SampleListener.java
@@ -0,0 +1,17 @@
+package me.soyeon.springpractice;
+
+import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+
+public class SampleListener implements ApplicationListener<ApplicationStartingEvent> {
+
+
+    @Override
+    public void onApplicationEvent(ApplicationStartingEvent applicationStartingEvent) {
+        System.out.println("=======================");
+        System.out.println("Application is starting");
+        System.out.println("=======================");
+    }
+}
